a129d3871f77bfcec33fa824798433349d162cef
tpcstld <jerryjiang1128@gmail.com>
2014-03-21 23:02:38 -0400

    Clean up

--- 2048/2048/src/main/java/com/tpcstld/twozerogame/MainActivity.java
+++ 2048/2048/src/main/java/com/tpcstld/twozerogame/MainActivity.java
@@ -2,11 +2,9 @@ package com.tpcstld.twozerogame;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.Window;
-import android.view.WindowManager;
 
 public class MainActivity extends ActionBarActivity {
 
