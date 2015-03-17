48fd40f4b650e8a34f3a1246d60a299b54999d67
Paul Woitaschek <woitaschek@posteo.de>
2015-03-17 15:24:55 +0100

    Since we use ActionBarActivity use correct support method

--- 2048/2048/src/main/java/com/tpcstld/twozerogame/MainActivity.java
+++ 2048/2048/src/main/java/com/tpcstld/twozerogame/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         view = new MainView(getBaseContext());
 
         SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(this);
